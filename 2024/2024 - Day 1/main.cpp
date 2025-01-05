1cdf0a5720adaccdc05096e3f04df81004021858
ChrisEverettAudioDesign <35993924+ChrisEverettAudioDesign@users.noreply.github.com>
2025-01-05 21:43:58 +0000

    Part One Solved!

--- 2024/2024 - Day 1/main.cpp
+++ 2024/2024 - Day 1/main.cpp
@@ -3,87 +3,57 @@
 #include <fstream>
 #include <algorithm>
 #include <numeric>
+#include <string>
 
 
 using namespace std;
 
-
 void listStorage()
 {
 	ifstream inputFile("inputfile.txt");
-	vector<char> leftList;
-	vector<char> rightList;
+	vector<int> leftList;
+	vector<int> rightList;
 	vector<int> coordinateSum;
 
-	char symbolInput;
-	int characterPostion = 0;
-	int globalCharacterPosition = 0;
+
+	// char symbolInput; - since changing to an int vector there was no need for this.
+	// int characterPostion = 0; - since changing to an int vector there was no need for this.
+
 	int coordinateGlobalSum = 0;
+	const int lineCount = 1000;
+	int symbolInputLeft;
+	int symbolInputRight;
+	
+	// changing the while loop to a for loop and adding the line count int gave the correct answer.
 
-	while (inputFile.get(symbolInput))
+	for(int i = 0; i < lineCount; i++)
 	{
-			
-			if (characterPostion <= 4)
-			{
-				leftList.push_back(symbolInput);
-			}
-			else if (characterPostion >= 8 && characterPostion < 13)
-			{
-				rightList.push_back(symbolInput);
-			}
-			else if (characterPostion >= 13)
-			{
-				characterPostion = -1;
+		inputFile >> symbolInputLeft;
+		inputFile >> symbolInputRight;
 
-				sort (leftList.begin() + leftList.size() - 5, leftList.end());
-				sort (rightList.begin() + rightList.size() - 5, rightList.end());
-			}
-			else {}
-			characterPostion++;
-			globalCharacterPosition++;  //mainly a debug variable can probably be ommited 
+		leftList.push_back(symbolInputLeft);
+		rightList.push_back(symbolInputRight);
+
 	}
 	
-	// This last sort function outside of the loop is because the last 5 numbers weren't being caught by the logic. (I haven't got a clue why) - FIXED
-	// Because I left a space off the end string in line 1000 of the inputfile.txt the while loop wasn't reaching 13 for the final iteration of the loop.
-	// Causing the logic to fail.
+	sort(leftList.begin(), leftList.end());
+	sort(rightList.begin(), rightList.end());
 
-		//sort(leftList.begin() + leftList.size() - 5, leftList.end());
-		//sort(rightList.begin() + rightList.size() - 5, rightList.end());
+	// The old way of sorting when the vector was a character - I missunderstood the problem.  But im still not sure at why the above sort worked.
+
+	/*sort(leftList.begin() + leftList.size() - 5, leftList.end());
+	sort(rightList.begin() + rightList.size() - 5, rightList.end());*/
+	
 
 	inputFile.close();
 
-	for (int i = 0; i < leftList.size(); i++)
+	for (int i = 0; i < lineCount; i++)
 	{
-		int leftValue = leftList[i];
-		int rightValue = rightList[i];
-		int difference = abs(leftValue - rightValue);
-		coordinateSum.push_back(difference);
+		coordinateGlobalSum += abs(leftList[i] - rightList[i]);
 	}
-
-	// Below is the original way that I coded the "if left is lower than right value/vice versa" issue.  This was before I knew about abs being a THING?!	
-	
-		//for (int itr = 0; itr != rightList.size(); itr++)
-		//{
-		//	if (leftList.at(itr) < rightList.at(itr))
-		//	{
-		//		coordinateSum.push_back (rightList.at(itr) - leftList.at(itr));
-		//	}
-		//	else
-		//	{
-		//		coordinateSum.push_back(leftList.at(itr) - rightList.at(itr));
-		//	}
-		//	
-		//}
-
-	// Could the last 5 numbers not be getting added properly?
 
 	cout << leftList.size() << endl;
 	cout << rightList.size() << endl;
-
-	coordinateGlobalSum = accumulate(coordinateSum.begin(), coordinateSum.end(), coordinateGlobalSum);
-	
-	
-
 
 	cout << coordinateGlobalSum << endl;
 }
@@ -98,3 +68,45 @@ int main()
 	
 	return 0;
 }
+
+
+
+//  Old way of getting information from the file.
+
+	/*while (inputFile.get(symbolInput))
+	{
+
+		if (characterPostion <= 4)
+			{
+				leftList.push_back(symbolInput);
+			}
+			else if (characterPostion >= 8 && characterPostion < 13)
+			{
+				rightList.push_back(symbolInput);
+			}
+			else if (characterPostion >= 13)
+			{
+				characterPostion = -1;
+				sort(leftList.begin() + leftList.size() - 5, leftList.end());
+				sort(rightList.begin() + rightList.size() - 5, rightList.end());
+
+			}
+			else {}
+			characterPostion++;
+
+	}*/
+
+	// Below is the original way that I coded the "if left is lower than right value/vice versa" issue.  This was before I knew about abs being a THING?!	
+
+		//for (int itr = 0; itr != rightList.size(); itr++)
+		//{
+		//	if (leftList.at(itr) < rightList.at(itr))
+		//	{
+		//		coordinateSum.push_back (rightList.at(itr) - leftList.at(itr));
+		//	}
+		//	else
+		//	{
+		//		coordinateSum.push_back(leftList.at(itr) - rightList.at(itr));
+		//	}
+		//	
+		//}
