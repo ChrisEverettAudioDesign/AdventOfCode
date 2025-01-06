d03a64f3c8d6a717e56ee60a179361668bb51c5a
ChrisEverettAudioDesign <mail@chriseverettaudiodesign.co.uk>
2025-01-06 17:13:10 +0000

    PART 2 Fix

--- 2024/2024 - Day 1/main.cpp
+++ 2024/2024 - Day 1/main.cpp
@@ -10,19 +10,22 @@ using namespace std;
 
 void listStorage()
 {
+	// LEARNINGS = I HAD A FULL ON MAJOR CONFUSION - I was taking the example too literally - I was treating like numbers like individual numbers rather than the full 5 digit int.
+	// Any code that I do needs to follow the theory of -  "it's important to write code that works for any dataset, not just the example one" - Gergs 6th Jan 2025.
+
+	// char symbolInput; - since changing to an int vector there was no need for this.
+	// int characterPostion = 0; - since changing to an int vector there was no need for this.
+
 	ifstream inputFile("inputfile.txt");
 	vector<int> leftList;
 	vector<int> rightList;
 	vector<int> coordinateSum;
 
-
-	// char symbolInput; - since changing to an int vector there was no need for this.
-	// int characterPostion = 0; - since changing to an int vector there was no need for this.
-
 	int coordinateGlobalSum = 0;
 	const int lineCount = 1000;
-	int symbolInputLeft;
-	int symbolInputRight;
+	int symbolInputLeft = 0;
+	int symbolInputRight = 0;
+	int singularityScore = 0;
 	
 	// changing the while loop to a for loop and adding the line count int gave the correct answer.
 
@@ -33,19 +36,35 @@ void listStorage()
 
 		leftList.push_back(symbolInputLeft);
 		rightList.push_back(symbolInputRight);
-
 	}
 	
 	sort(leftList.begin(), leftList.end());
 	sort(rightList.begin(), rightList.end());
 
-	// The old way of sorting when the vector was a character - I missunderstood the problem.  But im still not sure at why the above sort worked.
-
-	/*sort(leftList.begin() + leftList.size() - 5, leftList.end());
-	sort(rightList.begin() + rightList.size() - 5, rightList.end());*/
-	
-
 	inputFile.close();
+
+	for (int leftIndex = 0; leftIndex < leftList.size(); leftIndex++)
+	{
+		int foundCount = 0;
+		int leftSideCurrentNumber = leftList[leftIndex];
+		
+
+		for (int rightIndex = 0; rightIndex < rightList.size(); rightIndex++)
+		{
+			int rightSideCurrentNumber = rightList[rightIndex];
+
+			if (leftSideCurrentNumber == rightSideCurrentNumber)
+			{
+				foundCount += 1;
+			}	
+		}
+		
+		singularityScore += leftSideCurrentNumber * foundCount;
+
+
+		//singularityScore = count(rightList.begin(), rightList.end(), leftList.at(i));
+	}
+
 
 	for (int i = 0; i < lineCount; i++)
 	{
@@ -56,6 +75,7 @@ void listStorage()
 	cout << rightList.size() << endl;
 
 	cout << coordinateGlobalSum << endl;
+	cout << singularityScore << endl;
 }
 
 
@@ -95,6 +115,11 @@ int main()
 			characterPostion++;
 
 	}*/
+
+	// The old way of sorting when the vector was a character - I missunderstood the problem.
+
+	/*sort(leftList.begin() + leftList.size() - 5, leftList.end());
+	sort(rightList.begin() + rightList.size() - 5, rightList.end());*/
 
 	// Below is the original way that I coded the "if left is lower than right value/vice versa" issue.  This was before I knew about abs being a THING?!	
 
