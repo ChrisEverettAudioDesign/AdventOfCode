ed276444d1d150682c39206d8e742fa26ba55299
ChrisEverettAudioDesign <35993924+ChrisEverettAudioDesign@users.noreply.github.com>
2025-01-12 23:11:31 +0000

    More work on Day 2 Part 2

--- 2024/2024 - Day 2/main.cpp
+++ 2024/2024 - Day 2/main.cpp
@@ -23,7 +23,8 @@ void safeReports()
 	int num = 0;
 	int numOfSafeReports = 0;
 	bool absFail = false;
-	
+	int numOfAbsFails = 0;
+	int failLocation = 0;
 
 	// Count the number of intergers in the line.
 	while (getline(inputFile, line))
@@ -34,30 +35,51 @@ void safeReports()
 			reportInput.push_back(num);
 		}
 
+
+
+
 		for (int itr = 0; itr < reportInput.size() - 1; itr++)
 		{
+			
 			int absResult = 0;
-			absFail = false;
 			absResult = abs(reportInput[itr] - reportInput[itr+1]);
 			
-			if (absResult > 3 || absResult == 0)
+			if (absResult > 3 || absResult == 0  && numOfAbsFails <= 1)
 			{
-				absFail = true;
+				numOfAbsFails++;
+				failLocation = itr;				
+				reportInput.erase(reportInput.begin() + failLocation);
+				reportInput.resize(reportInput.size());
+				itr = -1;
+			}
+
+			else if (numOfAbsFails > 1)
+			{
+				reportInput.clear();
 				break;
 			}
-		}
+			else {}
 
-		if (is_sorted(reportInput.begin(), reportInput.end()) == true && absFail == false)
+		}
+			
+		if (is_sorted(reportInput.begin(), reportInput.end()) == true && numOfAbsFails <= 1)
 		{
 			numOfSafeReports++;
 		}
-		else if (is_sorted(reportInput.rbegin(), reportInput.rend()) == true && absFail == false)
+		else if (is_sorted(reportInput.rbegin(), reportInput.rend()) == true && numOfAbsFails <= 1)
 		{
 			numOfSafeReports++;
 		}
-		else
-		{ }
+		else if (is_sorted(reportInput.begin(), reportInput.end()) == false && numOfAbsFails <= 1)
+		{
+			numOfSafeReports++;
+		}
+		else if (is_sorted(reportInput.rbegin(), reportInput.rend()) == false && numOfAbsFails <= 1)
+		{
+			numOfSafeReports++;
+		}
 
+		numOfAbsFails = 0;
 		reportInput.clear();
 		
 	}
